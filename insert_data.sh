b4cae673e8cabe477458c807979d3e2914568160
gros-pataplouf <sceres@pm.me>
2023-03-24 20:18:42 +0100

    corrections to insert-data.sh

--- insert_data.sh
+++ insert_data.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#!/bin/bash
 
 if [[ $1 == "test" ]]
 then
@@ -8,7 +8,7 @@ else
 fi
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
-echo $($PSQL "TRUNCATE TABLE games, teams;")
+echo $($PSQL "TRUNCATE TABLE games, teams")
 
 LINENR=1
 cat games.csv | while IFS="," read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPONENT_GOALS
@@ -17,19 +17,21 @@ do
 if [[ $LINENR -gt 1 ]]
   then
     #insert both winner and opponent into teams table if not found
-    TEAM1=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
-    if [[ -z $TEAM1 ]]
-      then echo $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+    WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
+    if [[ -z $WINNER_ID ]]
+      then 
+      echo $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     fi
-    
-    TEAM2=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
-    if [[ -z $TEAM2 ]]
-      then echo $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      
+    OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+    if [[ -z $OPPONENT_ID ]] 
+      then 
+      echo $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
+
     fi
-    #complete the games table
-    #year, round, winner_id, opponent_id, winner_goals, opponent_goals
-    echo $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals)
-      VALUES($YEAR, '$ROUND', $($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'"), $($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'"), $WINNER_GOALS, $OPPONENT_GOALS)")
+    echo completing games $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES($YEAR,'$ROUND',$WINNER_ID,$OPPONENT_ID,$WINNER_GOALS,$OPPONENT_GOALS)")
   fi
   LINENR=$(($LINENR+1))
 done

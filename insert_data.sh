aef2ca8b58e46c7dbac72106a24febe1917f446b
gros-pataplouf <sceres@pm.me>
2023-03-24 15:32:19 +0100

    corrections

--- insert_data.sh
+++ insert_data.sh
@@ -19,16 +19,16 @@ if [[ $LINENR -gt 1 ]]
     #insert both winner and opponent into teams table if not found
     TEAM1=$($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'")
     if [[ -z $TEAM1 ]]
-      then INSERT1=$($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
+      then echo $($PSQL "INSERT INTO teams(name) VALUES('$WINNER')")
     fi
     
     TEAM2=$($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'")
     if [[ -z $TEAM2 ]]
-      then INSERT2=$($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
+      then echo $($PSQL "INSERT INTO teams(name) VALUES('$OPPONENT')")
     fi
     #complete the games table
     #year, round, winner_id, opponent_id, winner_goals, opponent_goals
-    INSERTGAME=$($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals)
+    echo $($PSQL "INSERT INTO games(year, round, winner_id, opponent_id, winner_goals, opponent_goals)
       VALUES($YEAR, '$ROUND', $($PSQL "SELECT team_id FROM teams WHERE name='$WINNER'"), $($PSQL "SELECT team_id FROM teams WHERE name='$OPPONENT'"), $WINNER_GOALS, $OPPONENT_GOALS)")
   fi
   LINENR=$(($LINENR+1))
